b7c043f2e9664f8d312e5385a9c0c90c3170d0b5
ipa-rwu <ruichao.wu@ipa.fraunhofer.de>
2020-08-03 23:27:15 +0200

    cleanup

--- controllers/modify/main.py
+++
@@ -1,9 +0,0 @@
-from drivers.ur5e_driver import UR5e_driver
-
-ur5e_driver = UR5e_driver()
-def main():
-    joints = ur5e_driver.get_joint()
-    print joints
-
-if __name__ == "__main__":
-    main()
